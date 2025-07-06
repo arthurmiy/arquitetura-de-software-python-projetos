5eb9430ad26bface5eb9a9cfb657ce1c1c922bbd
Arthur Miyazaki <arthurmiy@gmail.com>
2025-07-06 17:54:17 -0300

    Smart-view: Criada função para adição de atendente; Criada variável para armazenamento dos dados das pessoas atendentes

--- smart-view/app.py
+++ smart-view/app.py
@@ -1,4 +1,23 @@
 import tkinter as tk
+from tkinter import messagebox
+
+# Lista com os dados das pessoas atendentes
+atendentes = []
+
+# Função para adicionar atendente
+def adicionar_atendente():
+    nome = entrada_nome.get().strip()
+    
+    if not nome:
+        messagebox.showwarning("Nome vazio", "Digite um nome!")
+        return
+    
+    if nome in [a["nome"] for a in atendentes]:
+        messagebox.showinfo("Duplicado", "Atendente já existe.")
+        return
+
+    atendentes.append({"nome": nome, "vendas": 0})
+    entrada_nome.delete(0, tk.END)
 
 # Interface principal
 janela = tk.Tk()
