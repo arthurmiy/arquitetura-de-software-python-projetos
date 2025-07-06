2ef0c10e0b4e1b5e23e5ff9cd9c39821a4706259
Arthur Miyazaki <arthurmiy@gmail.com>
2025-07-06 17:42:20 -0300

    smart-view: Criação de janela e campo de texto

---
+++ smart-view/app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# Interface principal
+janela = tk.Tk()
+janela.title("Controle de Vendas – Smart View")
+
+entrada_nome = tk.Entry(janela)
+entrada_nome.pack(pady=5)
+
+janela.mainloop()
